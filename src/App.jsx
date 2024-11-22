ec7e0e6ac69a186654797a5f7c6fd8c478a51a76
s-a-c-h-i-n <sachinrkamath18@gmail.com>
2024-11-22 00:09:23 -0500

    misc changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SurveyPage from './Components/SurveyPage/SurveyPage'
 import Survey from './Components/Survey/Survey'
 import LandingPage from './Components/LandingPage/landingPage'
-import NavBar from './Components/navBar'
+import NavBar from './Components/Navbar/navBar'
 import Navigation from './routes'
 import { BrowserRouter } from 'react-router-dom'
 
--- src/Components/Navbar/navBar.jsx
+++ src/Components/Navbar/navBar.jsx
@@ -1,6 +1,6 @@
 import {React} from 'react'
 import { Link } from 'react-router-dom';
-import './Survey/Survey.css'
+import '../Survey/Survey.css'
 
 const NavBar = () => {
     return (
